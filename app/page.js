9388895efc146399fce2fa2c1034768828c4b241
sther <estheradeyemi796@gmail.com>
2023-09-22 19:52:26 +0100

    Resolved issue

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Alerts from '@/components/alerts/Alerts'
 import MainSection from '@/components/main-section/MainSection'
-import Section from '@/components/sections/section'
+import Section from '@/components/sections/Section';
 import React from 'react'
 
 const Main = () => {
