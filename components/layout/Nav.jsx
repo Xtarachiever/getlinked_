4c8a72b9d33f06bd965c6349f9d370dc0b25af62
Adeyemi Esther <estheradeyemi796@gmail.com>
2023-09-24 06:43:55 +0100

    Update Navigation

--- components/layout/Nav.jsx
+++ components/layout/Nav.jsx
@@ -23,7 +23,7 @@ const Nav = () => {
             <div className="flex links">
                 <Link href={'/#timeline'} className={`${pathName === '/#timeline' ? 'active' : ''}`} onClick={()=>setOpen(false)}>Timeline</Link>
                 <Link href={'/'} onClick={()=>setOpen(false)} className={`${pathName === '/' ? 'active' : ''}`}>Overview</Link>
-                <Link href={'#faqs'} className={`${pathName === '/#faqs' ? 'active' : ''}`} onClick={()=>setOpen(false)}>FAQS</Link>
+                <Link href={'/#faqs'} className={`${pathName === '/#faqs' ? 'active' : ''}`} onClick={()=>setOpen(false)}>FAQS</Link>
                 <Link href={'/contact'} className={`${pathName === '/contact' ? 'active' : ''}`} onClick={()=>setOpen(false)}>Contact</Link>
             </div>
             <Link href={'/register'} onClick={()=>setOpen(false)}
